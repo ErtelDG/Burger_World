e5549fc34b733f4f87a6e353c8329b9a8b6d6db0
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-09-28 18:46:23 +0200

    break

--- build/js/script.js
+++ build/js/script.js
@@ -32,6 +32,8 @@ let menuIngredients = [
 let menuPrice = [
     10.9, 12.9, 12.9, 13.9, 14.9, 15.9, 3.9, 4.4, 4.9, 6.9, 13.9, 13.9, 13.9,
 ];
+let basketMenu = [];
+let basketPrice = [];
 //render menu list
 function menuList() {
     let renderListMain = document.getElementById("renderMenus");
@@ -46,13 +48,13 @@ function menuList() {
             renderListMain.innerHTML += `
           <div class="m-4 p-4 border-solid border-2 relative flex">
                            <div class="w-8/12">
-                              <p class="text-lg mb-4 font-bold">${nameMenu}</p>
+                              <p class="text-lg mb-4 font-bold id="nameMenu${i}">${nameMenu}</p>
                               <p class="mb-2">${ingredientsMenu}</p>
                            </div>
                            <div class="w-4/12 flex justify-end items-end">
-                              <p class="text-orange-400 text-3xl font-bold">${priceMenu.toFixed(2)} €</p>
+                              <p class="text-orange-400 text-3xl font-bold id="priceMenu${i}">${priceMenu.toFixed(2)} €</p>
                            </div>
-                           <div class="absolute top-0 right-0 border-b-2 border-l-2 hover:bg-orange-100"><svg
+                           <div onclick="add(menuName[${i}], menuPrice[${i}])" class="absolute top-0 right-0 border-b-2 border-l-2 hover:bg-orange-100"><svg
                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                                  stroke="currentColor" class="w-8 h-8">
                                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -61,4 +63,10 @@ function menuList() {
                         </div>`;
         }
     }
+}
+function add(menuNameId, menuPriceId) {
+    let menuNameInArray = menuNameId;
+    let menuPriceInArray = menuPriceId;
+    basketMenu.push(menuNameInArray);
+    basketPrice.push(menuPriceInArray);
 }
--- build/script.ts
+++ build/script.ts
@@ -32,6 +32,9 @@ let menuPrice: number[] = [
    10.9, 12.9, 12.9, 13.9, 14.9, 15.9, 3.9, 4.4, 4.9, 6.9, 13.9, 13.9, 13.9,
 ];
 
+let basketMenu: string[] = [];
+let basketPrice: number[] = [];
+
 //render menu list
 
 function menuList() {
@@ -48,15 +51,15 @@ function menuList() {
          renderListMain.innerHTML += `
           <div class="m-4 p-4 border-solid border-2 relative flex">
                            <div class="w-8/12">
-                              <p class="text-lg mb-4 font-bold">${nameMenu}</p>
+                              <p class="text-lg mb-4 font-bold id="nameMenu${i}">${nameMenu}</p>
                               <p class="mb-2">${ingredientsMenu}</p>
                            </div>
                            <div class="w-4/12 flex justify-end items-end">
-                              <p class="text-orange-400 text-3xl font-bold">${priceMenu.toFixed(
-                                 2
-                              )} €</p>
+                              <p class="text-orange-400 text-3xl font-bold id="priceMenu${i}">${priceMenu.toFixed(
+            2
+         )} €</p>
                            </div>
-                           <div class="absolute top-0 right-0 border-b-2 border-l-2 hover:bg-orange-100"><svg
+                           <div onclick="add(menuName[${i}], menuPrice[${i}])" class="absolute top-0 right-0 border-b-2 border-l-2 hover:bg-orange-100"><svg
                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
                                  stroke="currentColor" class="w-8 h-8">
                                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -65,4 +68,11 @@ function menuList() {
                         </div>`;
       }
    }
+}
+
+function add(menuNameId: string, menuPriceId: number) {
+   let menuNameInArray: string = menuNameId;
+   let menuPriceInArray: number = menuPriceId;
+   basketMenu.push(menuNameInArray);
+   basketPrice.push(menuPriceInArray);
 }
