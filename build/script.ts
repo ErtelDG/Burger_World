5426b62f4aade37cb4f9f47f475161fbe7b3579c
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-09-30 11:09:42 +0200

    logic_implated

--- build/script.ts
+++ build/script.ts
@@ -37,7 +37,6 @@ let basketAmount: number[] = [];
 let basketPrice: number[] = [];
 
 //render menu list
-
 function menuList() {
    let renderListMain = document.getElementById("renderMenus");
    if (renderListMain != null) {
@@ -50,7 +49,7 @@ function menuList() {
 
       if (renderListMain != null) {
          renderListMain.innerHTML += `
-          <div class="m-4 p-4 border-solid border-2 relative flex">
+          <div class="m-4 p-4 border-solid border-2 relative flex h-32">
                            <div class="w-8/12">
                               <p class="text-lg mb-4 font-bold id="nameMenu${i}">${nameMenu}</p>
                               <p class="mb-2">${ingredientsMenu}</p>
@@ -71,6 +70,110 @@ function menuList() {
    }
 }
 
+//render basket
+function basket() {
+   let basket = document.getElementById("renderbasket");
+
+   if (basket != null) {
+      basket.innerHTML = " ";
+      for (let i = 0; i < basketMenu.length; i++) {
+         let totalPriceOneMenu = (basketPrice[i] * basketAmount[i]).toFixed(2);
+         basket.innerHTML += `
+      <div class="p-2 text-sm border-2 my-2">
+         <div class="grid grid-cols-8">
+            <p class="font-bold col-span-1}">${basketAmount[i]}</p>
+            <p class="font-bold col-span-5">${basketMenu[i]}</p>
+            <p class="col-span-2 text-right font-semibold">${totalPriceOneMenu}€</p>
+         </div>
+         <div class="pt-4 grid-cols-8 flex justify-end">
+            <p  onclick="subAt1(${i})"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" class="w-8 h-8">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                     d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+               </svg>
+            </p>
+            <p onclick="addAt1(${i})"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" class="w-8 h-8">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                     d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+               </svg>
+            </p>
+         </div>
+      </div>
+      `;
+      }
+
+      //render shipping costs and total
+      let subtotal: number = 0;
+      let shippingCost: number = 4.9;
+      let total: number = 0;
+
+      for (let i = 0; i < basketAmount.length; i++) {
+         subtotal += basketPrice[i] * basketAmount[i];
+      }
+      if (subtotal >= 40) {
+         shippingCost = 0;
+      }
+      total = subtotal + shippingCost;
+
+      if (subtotal <= 40) {
+         basket.innerHTML += `
+                     <div class="text-base text-center bg-gray-200 p-2 my-2">
+                        <div class="">
+                           <p class="text-sm">Nur noch <b>${(
+                              40 - subtotal
+                           ).toFixed(
+                              2
+                           )} €</b> <br>bis zur kostenlosen Zustellung.</p>
+                        </div>
+                     </div>`;
+      }
+
+      basket.innerHTML += `
+      <div class="text-base text-center p-2 my-2">
+         <div class="flex justify-between">
+            <p>Zwischensumme</p>
+            <p>${subtotal.toFixed(2)} €</p>
+         </div>
+         <div class="flex justify-between">
+            <p>Lieferkosten</p>
+            <p>${shippingCost.toFixed(2)} €</p>
+         </div>
+         <div class="flex justify-between font-bold">
+            <p>Gesamt</p>
+            <p>${total.toFixed(2)} €</p>
+         </div>
+         <button class="bg-gray-200 hover:bg-gray-300 w-full font-bold my-4 py-2 px-4 rounded-full">
+            Bezahlen (${total.toFixed(2)} €)
+         </button>
+      </div>
+      `;
+   }
+
+   if (basketMenu.length <= 0) {
+      if (basket != null) {
+         basket.innerHTML = " ";
+         basket.innerHTML += `
+      <div class="p-2 text-sm">
+         <div class="text-center border-2 py-4">
+            <p class="flex justify-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                     d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+               </svg>
+            </p>
+            <p class="pb-2 font-semibold text-lg">Fülle deinen Warenkorb</p>
+            <p class="text-xs">Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle dein
+               Essen.</p>
+            </p>
+         </div>
+      </div>
+      `;
+      }
+   }
+}
+
+// add a menu to the basket
 function add(menuNameId: string, menuPriceId: number) {
    let menuNameInArray: string = menuNameId;
    let menuPriceInArray: number = menuPriceId;
@@ -91,18 +194,26 @@ function add(menuNameId: string, menuPriceId: number) {
       basketPrice.push(menuPriceInArray);
    }
 
+   //render basket
+   basket();
+
    // function to check is menu name in basket
    function myFunctionSearchMenuInBasket(value: string) {
       return value == menuNameInArray;
    }
 }
 
-//       {
-//          console.log("notFound");
-//          basketMenu.push(menuNameInArray);
-//          let startAmount: number = 1;
-//          basketAmount.push(startAmount);
-//          basketPrice.push(menuPriceInArray);
-//       }
-//    }
-// }
+function subAt1(index: number) {
+   basketAmount[index]--;
+   if (basketAmount[index] == 0) {
+      basketAmount.splice(index, 1);
+      basketMenu.splice(index, 1);
+      basketPrice.splice(index, 1);
+   }
+   basket();
+}
+
+function addAt1(index: number) {
+   basketAmount[index]++;
+   basket();
+}
