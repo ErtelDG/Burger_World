71d15d26b9772b440977541bfcec0567958c92be
DGE - Titan3070 <104428173+Titan3070@users.noreply.github.com>
2022-10-02 13:49:45 +0200

    finalAll

--- build/js/mainScript.js
+++ build/js/mainScript.js
@@ -5,30 +5,25 @@ function menuList() {
     if (renderListMain != null) {
         renderListMain.innerHTML = "";
     }
-    for (let i = 0; i < menuName.length; i++) {
-        let nameMenu = menuName[i];
-        let ingredientsMenu = menuIngredients[i];
-        let priceMenu = menuPrice[i];
-        if (renderListMain != null) {
-            renderListMain.innerHTML += renderMenuListTemplate(i, nameMenu, ingredientsMenu, priceMenu);
-        }
-    }
+    renderMenuesForMenuCard(renderListMain); //render menu list on the card
 }
 //render basket right side on the html => main function
 function basket() {
     let basket = document.getElementById("renderbasket");
+    //render when basket have menues
     if (basket != null) {
         basket.innerHTML = " ";
         basket.innerHTML = renderCloseXButtonByResponiseTemplate(); //close "X" button when side is responsive and it was need
         menusWhereAreInBasket(basket); //menues where are in the basket
         renderShippingCostsAndTotal(basket); //render shipping costs and total
     }
+    //render when basket have no menues
     if (basketMenu.length <= 0) {
         if (basket != null) {
             basket.innerHTML = " ";
             basket.innerHTML += renderNoteShoppingBasketIsEmptyTemplate(); //render Note Shopping basket is empty
         }
-        closeBasket100vwvh();
+        closeBasket100vwvh(); //auto close basket by responsive side
     }
     showBottomTopWhenAPositionInBasket();
 }
